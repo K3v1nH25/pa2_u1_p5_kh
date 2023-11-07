2b24ee9474e81f14dbbdeeda97e52663129206e3
DavidBoada08 <dfboadaa@uce.edu.ec>
2023-11-07 18:57:45 -0500

    mi primer commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5KhPsApplication.java
@@ -0,0 +1,22 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5KhPsApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5KhPsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		System.out.println("Soy kevin Hurtado y");
+		System.out.println("Pablo Simbaña");
+	}
+
+}
