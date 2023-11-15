4ffc232a545731de6c12f8afc2f7dfbb403e60e4
Hurtado Kevin <kdhurtado@uce.edu.ec>
2023-11-14 19:44:08 -0500

    taller6

---
+++ src/main/java/com/uce/edu/repository/modelo/Materia.java
@@ -0,0 +1,29 @@
+package com.uce.edu.repository.modelo;
+
+public class Materia {
+	private String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+	@Override
+	public String toString() {
+		return "Materia [nombre=" + nombre + ", codigo=" + codigo + ", numeroCreditos=" + numeroCreditos + "]";
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNumeroCreditos() {
+		return numeroCreditos;
+	}
+	public void setNumeroCreditos(String numeroCreditos) {
+		this.numeroCreditos = numeroCreditos;
+	}
+	private String  codigo;
+	private String numeroCreditos;
+}
