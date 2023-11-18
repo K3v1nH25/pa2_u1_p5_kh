1adce2f3c0df3b98fdcd68f22cc367c3d20eff00
Hurtado Kevin <kdhurtado@uce.edu.ec>
2023-11-17 19:49:21 -0500

    taller7

--- src/main/java/com/uce/edu/Pa2U1P5KhPsApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5KhPsApplication.java
@@ -1,19 +1,23 @@
 package com.uce.edu;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.ioc.di.Estudiante;
+import com.uce.edu.repository.modelo.Materia;
+import com.uce.edu.service.IMateriaService;
 
 @SpringBootApplication
 public class Pa2U1P5KhPsApplication implements CommandLineRunner {
 	
 	@Autowired
-	private Estudiante estudiante;
+	private IMateriaService iMateriaService;
+	
+	@Autowired
+	private Materia materia;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5KhPsApplication.class, args);
@@ -21,12 +25,33 @@ public class Pa2U1P5KhPsApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		this.estudiante.setNombre("Kevin y Pablo");
-		this.estudiante.setApellido("Hurtado y Simbaña");
-		this.estudiante.setCedula("1724693311");
-		this.estudiante.setSalario(new BigDecimal(100));
-		System.out.println(estudiante);
-	
+		
+		this.materia.setCodigo("M1");
+		this.materia.setNombre("P.Avanzada");
+		this.materia.setNumeroCreditos("10");
+		
+		this.iMateriaService.registrar(materia);
+		
+		System.out.println("Reporte inicio");
+		List<Materia> reporte = this.iMateriaService.buscarTodos();
+		for (Materia mate: reporte) {
+			System.out.println(mate);
+		}
+		
+		Materia mate = this.iMateriaService.buscar("M1");
+		System.out.println(mate);
+		
+		mate.setNumeroCreditos("20");
+		
+		this.iMateriaService.actualizar(mate);
+		
+		Materia mate2 = this.iMateriaService.buscar("M1");
+		System.out.println(mate2);
+		
+		this.iMateriaService.eliminar("M1");
+		
+		mate2 = this.iMateriaService.buscar("M1");
+		System.out.println(mate2);
 	}
 
 }
--- src/main/java/com/uce/edu/repository/MateriaRepositoryImpl.java
+++ src/main/java/com/uce/edu/repository/MateriaRepositoryImpl.java
@@ -1,34 +1,61 @@
 package com.uce.edu.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.uce.edu.repository.modelo.Materia;
 
 @Repository
 public class MateriaRepositoryImpl implements IMateriaRepository {
+	
+	private static List<Materia> base = new ArrayList<Materia>();
 
 	@Override
 	public Materia seleccionar(String codigo) {
 		// TODO Auto-generated method stub
-		return null;
+		System.out.println("seleccionanos el codigo "+codigo);
+		
+		for(Materia ma: base) {
+			if(ma.getCodigo().equals(codigo)) 
+				return ma;
+			
+				return null;
+			
+		}
+	
+		return new Materia();
 	}
 
 	@Override
 	public void insetar(Materia materia) {
 		// TODO Auto-generated method stub
-		
+		base.add(materia);
+		System.out.println("Se inserto "+materia);
 	}
 
 	@Override
 	public void actualizar(Materia materia) {
 		// TODO Auto-generated method stub
-		
+		this.borrar(materia.getCodigo());
+		this.insetar(materia);
+		System.out.println("Se actualiza: "+materia);
 	}
 
 	@Override
 	public void borrar(String codigo) {
 		// TODO Auto-generated method stub
-		
+		Materia mate = this.seleccionar(codigo);
+		base.remove(mate);
+		System.out.println("Se borra: "+codigo);
 	}
+
+	@Override
+	public List<Materia> seleccionarTodos() {
+		// TODO Auto-generated method stub
+		return base;
+	}
+
 
 }
